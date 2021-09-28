c967204b7c5a3af65115fb77fdbf087b48194798
h-nabata <64454988+h-nabata@users.noreply.github.com>
2021-09-29 00:26:42 +0900

    Create Covalentradius.c

---
+++ atomic/Covalentradius.c
@@ -0,0 +1,291 @@
+double radius(char *a){
+  if(strcmp(a,"HX") == 0){
+    return 0.32;
+  }
+  if(strcmp(a,"HeX") == 0){
+    return 0.28;
+  }
+  if(strcmp(a,"LiX") == 0){
+    return 1.28;
+  }
+  if(strcmp(a,"BeX") == 0){
+    return 0.96;
+  }
+  if(strcmp(a,"BX") == 0){
+    return 0.84;
+  }
+  if(strcmp(a,"CX") == 0){
+    return 0.76;
+  }
+  if(strcmp(a,"NX") == 0){
+    return 0.71;
+  }
+  if(strcmp(a,"OX") == 0){
+    return 0.66;
+  }
+  if(strcmp(a,"FX") == 0){
+    return 0.57;
+  }
+  if(strcmp(a,"NeX") == 0){
+    return 0.58;
+  }
+  if(strcmp(a,"NaX") == 0){
+    return 1.66;
+  }
+  if(strcmp(a,"MgX") == 0){
+    return 1.41;
+  }
+  if(strcmp(a,"AlX") == 0){
+    return 1.21;
+  }
+  if(strcmp(a,"SiX") == 0){
+    return 1.16;
+  }
+  if(strcmp(a,"PX") == 0){
+    return 1.07;
+  }
+  if(strcmp(a,"SX") == 0){
+    return 1.05;
+  }
+  if(strcmp(a,"ClX") == 0){
+    return 1.02;
+  }
+  if(strcmp(a,"ArX") == 0){
+    return 1.06;
+  }
+  if(strcmp(a,"KX") == 0){
+    return 2.03;
+  }
+  if(strcmp(a,"CaX") == 0){
+    return 1.76;
+  }
+  if(strcmp(a,"ScX") == 0){
+    return 1.7;
+  }
+  if(strcmp(a,"TiX") == 0){
+    return 1.6;
+  }
+  if(strcmp(a,"VX") == 0){
+    return 1.53;
+  }
+  if(strcmp(a,"CrX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"MnX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"FeX") == 0){
+    return 1.32;
+  }
+  if(strcmp(a,"CoX") == 0){
+    return 1.26;
+  }
+  if(strcmp(a,"NiX") == 0){
+    return 1.24;
+  }
+  if(strcmp(a,"CuX") == 0){
+    return 1.32;
+  }
+  if(strcmp(a,"ZnX") == 0){
+    return 1.22;
+  }
+  if(strcmp(a,"GaX") == 0){
+    return 1.22;
+  }
+  if(strcmp(a,"GeX") == 0){
+    return 1.2;
+  }
+  if(strcmp(a,"AsX") == 0){
+    return 1.19;
+  }
+  if(strcmp(a,"SeX") == 0){
+    return 1.2;
+  }
+  if(strcmp(a,"BrX") == 0){
+    return 1.2;
+  }
+  if(strcmp(a,"KrX") == 0){
+    return 1.16;
+  }
+  if(strcmp(a,"RbX") == 0){
+    return 2.2;
+  }
+  if(strcmp(a,"SrX") == 0){
+    return 1.95;
+  }
+  if(strcmp(a,"YX") == 0){
+    return 1.9;
+  }
+  if(strcmp(a,"ZrX") == 0){
+    return 1.75;
+  }
+  if(strcmp(a,"NbX") == 0){
+    return 1.64;
+  }
+  if(strcmp(a,"MoX") == 0){
+    return 1.54;
+  }
+  if(strcmp(a,"TcX") == 0){
+    return 1.47;
+  }
+  if(strcmp(a,"RuX") == 0){
+    return 1.46;
+  }
+  if(strcmp(a,"RhX") == 0){
+    return 1.42;
+  }
+  if(strcmp(a,"PdX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"AgX") == 0){
+    return 1.45;
+  }
+  if(strcmp(a,"CdX") == 0){
+    return 1.44;
+  }
+  if(strcmp(a,"InX") == 0){
+    return 1.42;
+  }
+  if(strcmp(a,"SnX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"SbX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"TeX") == 0){
+    return 1.38;
+  }
+  if(strcmp(a,"IX") == 0){
+    return 1.39;
+  }
+  if(strcmp(a,"XeX") == 0){
+    return 1.4;
+  }
+  if(strcmp(a,"CsX") == 0){
+    return 2.44;
+  }
+  if(strcmp(a,"BaX") == 0){
+    return 2.15;
+  }
+  if(strcmp(a,"LaX") == 0){
+    return 2.07;
+  }
+  if(strcmp(a,"CeX") == 0){
+    return 2.04;
+  }
+  if(strcmp(a,"PrX") == 0){
+    return 2.03;
+  }
+  if(strcmp(a,"NdX") == 0){
+    return 2.01;
+  }
+  if(strcmp(a,"PmX") == 0){
+    return 1.99;
+  }
+  if(strcmp(a,"SmX") == 0){
+    return 1.98;
+  }
+  if(strcmp(a,"EuX") == 0){
+    return 1.98;
+  }
+  if(strcmp(a,"GdX") == 0){
+    return 1.96;
+  }
+  if(strcmp(a,"TbX") == 0){
+    return 1.94;
+  }
+  if(strcmp(a,"DyX") == 0){
+    return 1.92;
+  }
+  if(strcmp(a,"HoX") == 0){
+    return 1.92;
+  }
+  if(strcmp(a,"ErX") == 0){
+    return 1.89;
+  }
+  if(strcmp(a,"TmX") == 0){
+    return 1.9;
+  }
+  if(strcmp(a,"YbX") == 0){
+    return 1.87;
+  }
+  if(strcmp(a,"LuX") == 0){
+    return 1.87;
+  }
+  if(strcmp(a,"HfX") == 0){
+    return 1.75;
+  }
+  if(strcmp(a,"TaX") == 0){
+    return 1.7;
+  }
+  if(strcmp(a,"WX") == 0){
+    return 1.62;
+  }
+  if(strcmp(a,"ReX") == 0){
+    return 1.51;
+  }
+  if(strcmp(a,"OsX") == 0){
+    return 1.44;
+  }
+  if(strcmp(a,"IrX") == 0){
+    return 1.41;
+  }
+  if(strcmp(a,"PtX") == 0){
+    return 1.36;
+  }
+  if(strcmp(a,"AuX") == 0){
+    return 1.36;
+  }
+  if(strcmp(a,"HgX") == 0){
+    return 1.32;
+  }
+  if(strcmp(a,"TlX") == 0){
+    return 1.45;
+  }
+  if(strcmp(a,"PbX") == 0){
+    return 1.46;
+  }
+  if(strcmp(a,"BiX") == 0){
+    return 1.48;
+  }
+  if(strcmp(a,"PoX") == 0){
+    return 1.4;
+  }
+  if(strcmp(a,"AtX") == 0){
+    return 1.5;
+  }
+  if(strcmp(a,"RnX") == 0){
+    return 1.5;
+  }
+  if(strcmp(a,"FrX") == 0){
+    return 2.6;
+  }
+  if(strcmp(a,"RaX") == 0){
+    return 2.21;
+  }
+  if(strcmp(a,"AcX") == 0){
+    return 2.15;
+  }
+  if(strcmp(a,"ThX") == 0){
+    return 2.06;
+  }
+  if(strcmp(a,"PaX") == 0){
+    return 2.0;
+  }
+  if(strcmp(a,"UX") == 0){
+    return 1.96;
+  }
+  if(strcmp(a,"NpX") == 0){
+    return 1.9;
+  }
+  if(strcmp(a,"PuX") == 0){
+    return 1.87;
+  }
+  if(strcmp(a,"AmX") == 0){
+    return 1.8;
+  }
+  if(strcmp(a,"CmX") == 0){
+    return 1.69;
+  }
+  return 2.00;
+}
