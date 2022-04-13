b8eba2359aadb19ec8393bf9ea1d1faa56c5c0b7
h-nabata <64454988+h-nabata@users.noreply.github.com>
2022-04-13 18:02:37 +0900

    Update trans_entropy_correction.jl

--- Julia/trans_entropy_correction.jl
+++ Julia/trans_entropy_correction.jl
@@ -7,16 +7,19 @@ function main()
     AMU_to_KG = 1.66053886e-27          # UNIT CONVERSION
     temperature = 298.15                # K
 
-    # M. Mammen, E. Shakhnovich, J. Deutch, and G. Whitesides, J. Org. Chem. 1998, 63, 12, 3821–3830. https://pubs.acs.org/doi/10.1021/jo970944f
+    ### Reference
+    # [1] M. Mammen, E. Shakhnovich, J. Deutch, and G. Whitesides, J. Org. Chem. 1998, 63, 12, 3821–3830. https://pubs.acs.org/doi/10.1021/jo970944f
 
     solv_molecular_vol = 125.412  # 1 / mol  (DMF ... Cavity volume = 125.412 ~ 126.089 Ang**3) (const.))
     solv_molarity = 10.763        # mol / L (const.)
-    molecular_mass = 194.21       # g / mol   73.09 ... DMF;  119.38 ... CHCl3
+    molecular_mass = 73.09        # g / mol   73.09 ... DMF;  119.38 ... CHCl3
     conc = 3.1                    # mol / L (the sum of concentration of solutes (const.))
 
+    # "For cubic molecules in a 3D cubic array, Cfree = 8; for spherical molecules, Cfree = 6.3" from ref. [1]
     v_free = 8 * ((1e27 / (solv_molarity * AVOGADRO_CONSTANT)) ^ (1/3) - solv_molecular_vol ^ (1/3)) ^ 3
     freespace = v_free * solv_molarity * AVOGADRO_CONSTANT * 1e-24
 
+    # eq. (6a) from ref. [1]
     lmda = ((2.0 * π * molecular_mass * AMU_to_KG * BOLTZMANN_CONSTANT * temperature) ^ 0.5) / PLANCK_CONSTANT
     ndens = conc * 1000 * AVOGADRO_CONSTANT / (freespace / 1000.0)
     entropy = GAS_CONSTANT * (2.5 + log(lmda ^ 3 / ndens))  # J / (mol*K)
