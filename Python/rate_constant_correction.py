219be2d16afb9537037e84a3ec01c6f2ff2eee72
h-nabata <64454988+h-nabata@users.noreply.github.com>
2022-04-22 19:30:30 +0900

    Update rate_constant_correction.py

--- Python/rate_constant_correction.py
+++ Python/rate_constant_correction.py
@@ -17,8 +17,9 @@ Z = (BOLTZMANN_CONSTANT * temperature / PLANCK_CONSTANT)
 k = Z * E
 
 # Wigner's tunneling correction (E. Wigner, Phys. Rev., 1932, 40, 749–759.)
-# 100.0 is the scaling factor to convert cm-1 into m-1
+# 100.0 is the scaling factor to convert cm-1 into m-1   # 1 [cm-1] ~ 1.986445683269303e-23 [J]
 wigner_factor = (PLANCK_CONSTANT * (abs(frequency) * 100.0 * SPEED_OF_LIGHT) / (BOLTZMANN_CONSTANT * temperature))
+
 # the transmission coefficient
 kappa = 1 + 1/24 * wigner_factor ** 2
 Z_corr = kappa * Z
